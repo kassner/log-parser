80d9248d564927e14e1af5fe0356fad8307bece5
Rafael Kassner <kassner@gmail.com>
2013-09-17 13:39:25 -0300

    NonZeroPositiveInteger tests

--- tests/Kassner/Tests/ApacheLogParser/Provider/NonZeroPositiveInteger.php
+++ tests/Kassner/Tests/ApacheLogParser/Provider/NonZeroPositiveInteger.php
@@ -7,23 +7,26 @@ class NonZeroPositiveInteger extends \PHPUnit_Framework_TestCase
 
     public function successProvider()
     {
-        /** @TODO test big numbers */
         return array(
             array('1'),
             array('1234'),
-            array('99999999')
+            array('99999999'),
+            array('100000000000000000000000')
         );
     }
 
     public function invalidProvider()
     {
-        /** @TODO test negative big numbers */
-        /** @TODO test doubles */
         return array(
             array('0'),
+            array('-1'),
             array('dummy 1234'),
             array('lala'),
-            array('-')
+            array('-'),
+            array('-100000000000000000000000'),
+            array('12.34'),
+            array('0.0'),
+            array('-0'),
         );
     }
 
