108851555de426f42ea2dc70a5ad809c74347e4b
Rafael Kassner <kassner@gmail.com>
2013-11-27 22:30:43 -0200

    Added test for #5: Blank referrers and agents

---
+++ tests/Kassner/Tests/ApacheLogParser/Issue/Issue5Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kassner\Teste\ApacheLogParser\Issue;
+
+use Kassner\ApacheLogParser\ApacheLogParser;
+
+class Issue5Test extends \PHPUnit_Framework_TestCase
+{
+
+    public function testReferersAndAgents()
+    {
+        $parser = new ApacheLogParser("%v:%p %h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"");
+        $entry = $parser->parse('www.example.com:80 ::1 - - [27/Oct/2013:06:27:33 +0000] "OPTIONS * HTTP/1.0" 200 126 "-" "Apache/2.2.22 (Ubuntu) (internal dummy connection)"');
+        $this->assertEquals('www.example.com', $entry->serverName);
+        $this->assertEquals('80', $entry->port);
+        $this->assertEquals('::1', $entry->host);
+        $this->assertEquals('-', $entry->logname);
+        $this->assertEquals('-', $entry->user);
+        $this->assertEquals('27/Oct/2013:06:27:33 +0000', $entry->time);
+        $this->assertEquals('OPTIONS * HTTP/1.0', $entry->request);
+        $this->assertEquals('200', $entry->status);
+        $this->assertEquals('126', $entry->sentBytes);
+        $this->assertEquals('-', $entry->HeaderReferer);
+        $this->assertEquals('Apache/2.2.22 (Ubuntu) (internal dummy connection)', $entry->HeaderUserAgent);
+    }
+
+}
