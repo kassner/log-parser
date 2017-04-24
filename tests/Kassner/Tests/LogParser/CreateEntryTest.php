8297e03f0f0d2707c2657cc6d2721189a8978634
Rafael Kassner <kassner@gmail.com>
2017-04-24 08:40:03 +0200

    Tests for LogParser::createEntry

---
+++ tests/Kassner/Tests/LogParser/CreateEntryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kassner\Tests\LogParser;
+
+use Kassner\LogParser\LogParser;
+use Kassner\Tests\LogParser\Entry\Fake as FakeEntry;
+
+class CreateEntryTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCreateEnty()
+    {
+        $parser = new LogParser('%h');
+        $entry = $parser->parse('66.249.74.132');
+
+        // Not using \stdClass::class because it's available on PHP5.5+ only
+        $this->assertInstanceOf('\stdClass', $entry);
+    }
+
+    public function testCreateEntyMocked()
+    {
+        $mock = $this->getMock('\Kassner\LogParser\LogParser', array('createEntry'), array('%h'));
+        $mock->expects($this->any())->method('createEntry')->willReturn(new FakeEntry());
+
+        $entry = $mock->parse('66.249.74.132');
+
+        // Not using \stdClass::class because it's available on PHP5.5+ only
+        $this->assertInstanceOf('\Kassner\Tests\LogParser\Entry\Fake', $entry);
+        $this->assertEquals($entry->host, '66.249.74.132');
+    }
+
+}
---
+++ tests/Kassner/Tests/LogParser/Entry/Fake.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kassner\Tests\LogParser\Entry;
+
+class Fake
+{
+
+    public $host;
+
+}
