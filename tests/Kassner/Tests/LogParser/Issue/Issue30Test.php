fec5dfd24374b194b751e76ced5747ad9321210b
Rafael Kassner <kassner@gmail.com>
2017-06-25 13:36:41 +0300

    Fix white spaces

--- tests/Kassner/Tests/LogParser/Issue/Issue30Test.php
+++ tests/Kassner/Tests/LogParser/Issue/Issue30Test.php
@@ -11,7 +11,7 @@ class Issue30Test extends \PHPUnit_Framework_TestCase
         $parser = new LogParser();
         $parser->setFormat('%h %l %u %t "%r" %>s %O "%{Referer}i" "%{User-Agent}i"');
         $entry = $parser->parse('127.0.0.1 - - [25/Jun/2017:10:14:35 +0000] "GET / HTTP/1.1" 200 11576 "-" "curl/7.47.0"');
-        
+
         $this->assertEquals('127.0.0.1', $entry->host);
         $this->assertEquals('-', $entry->logname);
         $this->assertEquals('-', $entry->user);
