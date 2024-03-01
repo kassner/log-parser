1e2b24c78ef51ae03fdd9e5b1f7edcf96578267e
Jonas Raoni Soares da Silva <github@raoni.org>
2024-03-01 12:55:01 +0300

    #64 Fixed warning regarding the deprecation of dynamic properties

--- src/SimpleLogEntry.php
+++ src/SimpleLogEntry.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Kassner\LogParser;
 
+use AllowDynamicProperties;
+
+#[AllowDynamicProperties]
 class SimpleLogEntry implements LogEntryInterface
 {
     /** @var int|null */
